58ce9824d51295f1ab564fa0162757a5fc61bbf8
Pavel <hobbbb@gmail.com>
2013-11-22 11:26:29 +0400

    start olimp

--- bin/app.pl
+++ bin/app.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/env perl
 use Dancer;
-use BugTracker;
+use StartOlimp;
 dance;
---
+++ lib/StartOlimp.pm
@@ -0,0 +1,12 @@
+package StartOlimp;
+
+use Dancer ':syntax';
+
+# load_app 'SCM::SPE';
+# load_app 'SPE::bugs', prefix => '/bugs';
+
+# any '/' => sub {
+#     return redirect "/bugs/?filter_status=todo";
+# };
+
+true;
