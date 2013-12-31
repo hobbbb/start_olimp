eafe83bf0b6f987b4407ef9507773c48b93ead6d
Hob <hobbbb@gmail.com>
2013-12-31 15:09:32 +0400

    core

--- lib/Record.pm
+++ lib/Record.pm
@@ -21,7 +21,7 @@ sub list {
     my ($class, $cond) = @_;
     $cond ||= {};
 
-    my $list = [ database->quick_select($class->TABLE, $cond) ];
+    my $list = [ database->quick_select($class->_table, $cond) ];
 
     if (scalar @$list == 1) {
         return wantarray ? map { $class->new($_) } @$list : $class->new($list->[0]);
@@ -35,7 +35,7 @@ sub count {
     my ($class, $cond) = @_;
     $cond ||= {};
 
-    return database->quick_count($class->TABLE, $cond);
+    return database->quick_count($class->_table, $cond);
 }
 
 sub create {
@@ -60,65 +60,9 @@ sub remove {
     }
 }
 
-sub _insert {
-    my ($class, %params) = @_;
-
-    %params = $class->clear_params(%params) or return;
-    if ($class->can('validate')) {
-        return unless $class->validate(%params);
-    }
-    my $res = database->quick_insert($class->TABLE, \%params);
-
-    return $res eq '0E0' ? undef : database->last_insert_id(undef, undef, undef, undef);
-}
-
-sub _update {
-    my ($self, %params) = @_;
-    return unless $self->id;
-
-    %params = $class->clear_params(%params) or return;
-    for my $k (keys %params) {
-        $self->$k($params{$k});
-    }
-    if ($self->can('validate')) {
-        return unless $self->validate(%params);
-    }
-    my $res = database->quick_update($self->TABLE, { id => $self->id }, { %$self });
-
-    return $res eq '0E0' ? undef : $self;
-}
-
-sub _delete {
-    my $self = shift;
-    my $res = database->quick_delete($self->TABLE, { id => $self->id });
-    return $res eq '0E0' ? undef : 1;
-}
-
-### Other methods
-
 sub as_vars {
     my $self = shift;
     return +{ %$self };
-}
-
-sub TABLE {
-    my ($invocant) = @_;
-    my $name = ref($invocant) || $invocant;
-    $name =~ s/^Record:://;
-    $name =~ s/::/_/g;
-    return $name;
-}
-
-sub clear_params {
-    my ($invocant, %params) = @_;
-    my $class = ref($invocant) || $invocant;
-
-    for my $k (keys %params) {
-        unless (grep(/^$k$/, $class->meta->get_attribute_list)) {
-            delete $params{$k};
-        }
-    }
-    return keys %params ? %params : undef;
 }
 
 sub merge_params {
@@ -133,6 +77,62 @@ sub merge_params {
     }
 
     return %merge;
+}
+
+### CORE
+
+sub _insert {
+    my ($class, %params) = @_;
+
+    %params = $class->_clear_params(%params) or return;
+    if ($class->can('validate')) {
+        return unless $class->validate(%params);
+    }
+    my $res = database->quick_insert($class->_table, \%params);
+
+    return $res eq '0E0' ? undef : database->last_insert_id(undef, undef, undef, undef);
+}
+
+sub _update {
+    my ($self, %params) = @_;
+    return unless $self->id;
+
+    %params = $self->_clear_params(%params) or return;
+    for my $k (keys %params) {
+        $self->$k($params{$k});
+    }
+    if ($self->can('validate')) {
+        return unless $self->validate(%params);
+    }
+    my $res = database->quick_update($self->_table, { id => $self->id }, { %$self });
+
+    return $res eq '0E0' ? undef : $self;
+}
+
+sub _delete {
+    my $self = shift;
+    my $res = database->quick_delete($self->_table, { id => $self->id });
+    return $res eq '0E0' ? undef : 1;
+}
+
+sub _clear_params {
+    my ($invocant, %params) = @_;
+    my $class = ref($invocant) || $invocant;
+
+    for my $k (keys %params) {
+        unless (grep(/^$k$/, $class->meta->get_attribute_list)) {
+            delete $params{$k};
+        }
+    }
+    return keys %params ? %params : undef;
+}
+
+sub _table {
+    my ($invocant) = @_;
+    my $name = ref($invocant) || $invocant;
+    $name =~ s/^Record:://;
+    $name =~ s/::/_/g;
+    return $name;
 }
 
 __PACKAGE__->meta->make_immutable();
