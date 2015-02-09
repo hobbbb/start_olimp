4c7363cda20c8b77be26bbd5e32e4e00a0b80313
Vasilyev Pavel <hobbbb@gmail.com>
2015-02-10 00:13:22 +0300

    ...

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-def Index(request):
+def index(request):
     return render_to_response('index.html', locals(), context_instance=RequestContext(request))
+
+def registration(request):
+    return render_to_response('registration.html', locals(), context_instance=RequestContext(request))
--- start_olimp/urls.py
+++ start_olimp/urls.py
@@ -6,9 +6,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'startolimp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    (r'^$', 'core.views.Index'),
-    # (r'^login/$', 'django.contrib.auth.views.login', {'template_name':'login.html'}),
-    # (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page':'/login/'}),
+    (r'^$', 'core.views.index'),
+    (r'^registration/$', 'core.views.registration'),
+    (r'^login/$', 'django.contrib.auth.views.login', {'template_name':'login.html'}),
+    (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page':'/'}),
 
     url(r'^admin/', include(admin.site.urls)),
 )
