c92355c5a974f2920281268d9aa3b0f971334d25
Vasilyev Pavel <hobbbb@gmail.com>
2015-02-12 12:09:21 +0300

    fix static

--- users/models.py
+++ users/models.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# https://github.com/jcugat/django-custom-user
+
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 from django.core.mail import send_mail
